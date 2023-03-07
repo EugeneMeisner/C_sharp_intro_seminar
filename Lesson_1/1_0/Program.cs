a398bf99b4cbf71fd7a4deacf7a4e74938320631
Eugene <reitelx2@gmail.com>
2023-03-07 17:17:58 +0300

    Task 1_0

---
+++ Lesson_1/1_0/Program.cs
@@ -0,0 +1,6 @@
+﻿// 0. Write a program that take a number as input and outputs
+// its square(the number multiplied by itself).
+Console.WriteLine("Write a number: ");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(num * num);
