a8356f5f08ba980692662368f95e89617a1be815
Eugene <reitelx2@gmail.com>
2023-03-07 20:16:35 +0300

    task 1_3

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Write a program that takes one number as input
+// (N), and the output shows all integers between -N and N.
+Console.WriteLine("Enter N: ");
+int num = int.Parse(Console.ReadLine()!);
+int count = -num;
+while (count != num)
+{
+    Console.WriteLine(count); 
+    if(num<0)
+    {
+        count--;
+    }
+    else count++;
+
+}
