4211012116b6c98d87f1a15e9a8feed973adb51e
Eugene <reitelx2@gmail.com>
2023-03-07 19:14:59 +0300

    task 1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. Write a program that takes two numbers as input and checks if
+// whether the first number is the square of the second.
+Console.WriteLine("Please enter number 1: ");
+
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Please enter number 2: ");
+
+int num2 = int.Parse(Console.ReadLine()!);
+
+if(num2 * num2 == num1)
+{
+    Console.WriteLine("the number 1 is the square of the number 2 ");
+}
+else
+{
+    Console.WriteLine("number 1 is not a square of number 2 ");
+}
