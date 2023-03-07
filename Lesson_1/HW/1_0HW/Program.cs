5435e754c42544a908c1df221a1a5590c9018836
Eugene <reitelx2@gmail.com>
2023-03-07 21:10:29 +0300

    task 1_0_HW

---
+++ Lesson_1/HW/1_0HW/Program.cs
@@ -0,0 +1,25 @@
+﻿// Write a program that takes two numbers as input and outputs which number
+// is greater and which is less.
+// a = 5; b=7 -> max=7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Enter a number a: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter a number b: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    Console.WriteLine("a=" + num1 + ";" + "b=" + num2 + "->" + "max=" + num1);
+}
+else if (num1 < num2)
+{
+    Console.WriteLine("a=" + num1 + ";" + "b=" + num2 + "->" + "max=" + num2);
+}
+else
+{
+    num1 = num2;
+    Console.WriteLine("Error, a=b number are quol");
+}
