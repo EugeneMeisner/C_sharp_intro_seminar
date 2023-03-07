e6d7d159f0b47eaf4dd2b6e111aeb45b3a1e96d8
Eugene <reitelx2@gmail.com>
2023-03-07 23:36:53 +0300

    task 1_2_HW

---
+++ Lesson_1/HW/1_2HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Write a program that takes a number as input and outputs whether the number is even 
+// (whether it is divisible by two without a remainder).
+// 4 -> yes
+// -3 -> no
+// 7 -> no
+Console.Write("Enter a number: ");
+int num = int.Parse(Console.ReadLine()!);
+int num1 = num %= 2;
+if (num1 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
