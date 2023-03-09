8eacbcb738e30837d954312254bf8819d8f4a8ed
Eugene <reitelx2@gmail.com>
2023-03-09 10:57:06 +0300

    1_3_HW

--- Lesson_1/HW/1_1HW/Program.cs
+++ Lesson_1/HW/1_1HW/Program.cs
@@ -12,10 +12,10 @@ int num2 = int.Parse(Console.ReadLine()!);
 Console.Write("Enter a number 3: ");
 int num3 = int.Parse(Console.ReadLine()!);
 
-int max=num1;
+int max = num1;
 
-if(num2>max) max=num2;
-if(num3>max) max=num3;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
 
-Console.Write(num1+","+num2+","+num3+"->"+max);
+Console.Write(num1 + "," + num2 + "," + num3 + "->" + max);
 
---
+++ Lesson_1/HW/1_3HW/Program.cs
@@ -0,0 +1,20 @@
+﻿// Write a program that takes a number (N) as input and outputs all even numbers from 1 to N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Clear();
+Console.WriteLine("Enter a number N: ");
+int num = int.Parse(Console.ReadLine()!);
+int count = 2;
+
+if (num > 1)
+{
+    while (count <= num)
+    {
+        Console.Write(count);
+        count = count + 2;
+    }
+}
+else
+{
+    Console.Write("error");
+}
