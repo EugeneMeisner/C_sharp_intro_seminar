d93555e86a2fc361d4aed30fada400a1c6b2013d
Eugene <reitelx2@gmail.com>
2023-03-12 11:27:01 +0300

    task 2_0

---
+++ Lesson_2/2_0/Program.cs
@@ -0,0 +1,12 @@
+﻿// Write a program that accepts a three-digit number at the input and at the output
+// show the last digit of that number. 
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+int thNum = new Random().Next(100, 1000);    //[100, 999)
+int result = TakeNum(thNum);
+Console.WriteLine(result);
