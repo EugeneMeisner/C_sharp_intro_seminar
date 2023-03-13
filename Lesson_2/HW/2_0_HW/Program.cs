496e50b84549e6a249f5f446a685e4da07b39fff
Eugene <reitelx2@gmail.com>
2023-03-13 22:15:24 +0300

    task 2_0_HW

---
+++ Lesson_2/HW/2_0_HW/Program.cs
@@ -0,0 +1,31 @@
+﻿// Write a program that takes a three-digit number 
+// as input and outputs the second digit of that number.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+void PrintMessage(string message)
+{
+    Console.WriteLine(message);
+}
+int InputNum()
+{
+     return int.Parse(Console.ReadLine());
+}
+int FirstNum(int arg)
+{
+    int getfirst =arg / 10;
+    return getfirst;
+}
+int SecondNum(int arg)
+{
+    int getSecond =arg % 10;
+    return getSecond;
+}
+
+PrintMessage("Enter a three-digit numbers: ");
+int num = InputNum();
+int delFirst = FirstNum(num);
+int delSecond= SecondNum(delFirst);
+PrintMessage("second digit of that number: " + delSecond);
+PrintMessage("==============================");
