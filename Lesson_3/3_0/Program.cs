f71845824af0f3f3773b3c6e4a3d358f2de51b4d
Eugene <reitelx2@gmail.com>
2023-03-14 23:07:38 +0300

    task_3_0

---
+++ Lesson_3/3_0/Program.cs
@@ -0,0 +1,30 @@
+﻿// 1. Write a program that takes as input the coordinates of a point (X and Y),
+// moreover, X ≠ 0 or Y ≠ 0 and gives the number of a quarter of the plane,
+// where this point is located.
+void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0)
+    {
+        Console.WriteLine("Error. Please enter coordinates, not a zero!");
+    }
+    else if (x > 0 && y > 0)
+    {
+        Console.WriteLine("quarter plane I");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("quarter plane II");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("quarter plane III");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("quarter plane IV");
+    }
+}
+System.Console.WriteLine("Please enter: ");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+Quarters(num1, num2);
