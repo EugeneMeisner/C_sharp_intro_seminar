12564dfaaaf840d2cd11d0a07bf5cd3f2f90daec
Eugene <reitelx2@gmail.com>
2023-03-19 21:25:28 +0300

    task_4_0

---
+++ Lesson_4/4_0/Program.cs
@@ -0,0 +1,12 @@
+﻿// Write a program that accepts
+//input number (A) and gives the sum of numbers from 1 to A.
+int SumNum(int num)
+{
+    int sum_all = 0;
+    for(int i = 1; i <= num; i++)
+        sum_all+=i;
+    return sum_all;
+}
+int val = int.Parse(Console.ReadLine()!);
+int result = SumNum(val);
+Console.WriteLine(result);
