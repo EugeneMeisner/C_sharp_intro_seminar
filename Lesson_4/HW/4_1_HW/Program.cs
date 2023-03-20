2929b1e1cf4fbec94c06f429be30f98f8dab7629
Eugene <reitelx2@gmail.com>
2023-03-20 10:59:04 +0300

    task_4_1_hw

---
+++ Lesson_4/HW/4_1_HW/Program.cs
@@ -0,0 +1,29 @@
+﻿// Write a loop that takes as input
+// two numbers (A and B) and raises the number A to natural degree B. 
+void PrintMsg(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+void PrintMsgInt(int msg)
+{
+    System.Console.WriteLine(msg);
+}
+int ReadInput()
+{
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+int GetNumbers()
+{   PrintMsg("Please enter A :");
+    int a = ReadInput();
+    PrintMsg("Please enter B :");
+    int b = ReadInput();
+    int res=1;
+    for (int i = 0; i < b; i++)
+    {
+        res*=a;
+    }
+    return res;
+}
+
+PrintMsgInt(GetNumbers());
