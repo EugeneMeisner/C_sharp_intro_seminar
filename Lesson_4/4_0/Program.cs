87061a9e74bb20f8a1e2fb11f885b7b864bb5b18
Eugene <reitelx2@gmail.com>
2023-03-20 08:42:23 +0300

    task_4_1

--- Lesson_4/4_0/Program.cs
+++ Lesson_4/4_0/Program.cs
@@ -3,8 +3,8 @@
 int SumNum(int num)
 {
     int sum_all = 0;
-    for(int i = 1; i <= num; i++)
-        sum_all+=i;
+    for (int i = 1; i <= num; i++)
+        sum_all += i;
     return sum_all;
 }
 int val = int.Parse(Console.ReadLine()!);
---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Write a program that accepts
+// a number as input and returns the number of digits in the number.
+void Print(string msg)
+{
+    Console.WriteLine(msg);
+}
+int Parse()
+{
+    int parse = int.Parse(Console.ReadLine()!);
+    return parse;
+}
+int Calc(int num)
+{
+    int i = 0;
+
+   while(num>0)
+    {
+       num /= 10;
+       i++;
+       
+    }
+    return i;
+}
+Print("Enter a number: ");
+int val = Parse();
+int result = Calc(val);
+Print($"{result}"); 
