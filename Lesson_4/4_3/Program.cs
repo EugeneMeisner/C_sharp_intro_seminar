bdc043a2bcd03f65a08ba3aa3d4c3a1978ad9722
Eugene <reitelx2@gmail.com>
2023-03-20 10:08:53 +0300

    task_4_3

---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Write a program that outputs an array of 8 elements filled with zeros and
+// units in random order.
+int GenerateRandom()
+{
+    int random = new Random().Next(2);  //[0,1)
+    return random;
+}
+void PrintArr(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+int[] RandArr()
+{
+    int[] arr = new int[8];
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = GenerateRandom();
+    }
+    return arr;
+}
+PrintArr(RandArr());
